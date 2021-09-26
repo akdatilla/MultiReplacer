c1f9321a552a8ab923758ebb6e844b8d35e6c1dc
akdatilla <91418345+akdatilla@users.noreply.github.com>
2021-09-26 17:02:11 +0300

    Update MultiReplacer.dpr

--- MultiReplacer.dpr
+++ MultiReplacer.dpr
@@ -1,4 +1,22 @@
-(*
+{ 
+ Multi Replacer
+ A very capable Windows Grep, search, replace and data extraction tool. www.multireplacer.com
+ 
+ Programmed by Atilla YARALI.  www.atillayarali.com
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ }
+ (*
 {$A8,B-,C+,D+,E-,F-,G+,H+,I+,J-,K-,L+,M-,N+,O+,P+,Q-,R-,S-,T-,U-,V+,W-,X+,Y+,Z1}
 {$MINSTACKSIZE $00004000}
 {$MAXSTACKSIZE $00100000}
